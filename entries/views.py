369bafe7ce1d4a4ef5c407d232b65c88885ea37f
Julian Aparicio <japaric4@uci.edu>
2023-08-14 08:45:24 -0700

    The diary is completely finished now! Using a base class, the url for the diary knows where to go when a user is logged out and logged back in. This functionality uses the login info from the django website. I also added flash messages that pop up when editing, deleting, or creating new diary entries. These are important to the user interface and make the diary easier to use. There is a lot of connectiong buttons with their urls too so the diary knows where to redirect too when a button is pushed. I can add and change to the style of the diary however I want now

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,6 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -9,28 +12,39 @@ from django.views.generic import (
 
 from .models import Entry
 
-class EntryListView(ListView):
+class LockedView(LoginRequiredMixin):
+    login_url = "admin:login"
+
+class EntryListView(LockedView, ListView):
     model = Entry
     queryset = Entry.objects.all().order_by("-date_created")
 
-class EntryDetailView(DetailView):
+
+class EntryDetailView(LockedView, DetailView):
     model = Entry
 
-class EntryCreateView(CreateView):
+
+class EntryCreateView(LockedView, SuccessMessageMixin, CreateView):
     model = Entry
     fields = ["title", "content"]
     success_url = reverse_lazy("entry-list")
+    success_message = "Your new entry was created!"
 
-class EntryUpdateView(UpdateView):
+
+class EntryUpdateView(LockedView, SuccessMessageMixin, UpdateView):
     model = Entry
     fields = ["title", "content"]
+    success_message = "Your entry was updated!"
 
     def get_success_url(self):
-        return reverse_lazy(
-            "entry-detail",
-            kwargs={"pk": self.entry.id}
-        )
+        return reverse_lazy("entry-detail", kwargs={"pk": self.object.pk})
 
-class EntryDeleteView(DeleteView):
+
+class EntryDeleteView(LockedView, SuccessMessageMixin, DeleteView):
     model = Entry
     success_url = reverse_lazy("entry-list")
+    success_message = "Your entry was deleted!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
