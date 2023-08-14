a2ae882a3f240ed59affddb199bff18da0ff7a01
Julian Aparicio <japaric4@uci.edu>
2023-08-13 23:17:39 -0700

    Now that there is color and and easier functionality to access diary entries on the web server, the next step is to make it easier to add new diary entries on the front end.

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
 
-# Create your views here.
+from .models import Entry
+
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-date_created")
+
+class EntryDetailView(DetailView):
+    model = Entry
